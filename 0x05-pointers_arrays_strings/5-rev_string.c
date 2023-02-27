bbf35f4c3c240616c2d5200435e9bd1eea79585a
Reinstorm <joemugoh215@gmail.com>
2023-02-27 14:35:07 -0800

    lost

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string to reverse
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char temp;
+
+	while (s[len] != '\0')
+		len++;
+
+	len--;
+
+	while (i < len)
+	{
+		temp = s[i];
+		s[i] = s[len];
+		s[len] = temp;
+
+		i++;
+		len--;
+	}
+}
