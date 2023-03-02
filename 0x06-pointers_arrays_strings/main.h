4f99b126bedff2a59cf5ea219e7bad87980e9f5e
Reinstorm <joemugoh215@gmail.com>
2023-03-02 13:51:12 -0800

    bad

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *_strncpy(char *dest, char *src, int n);
+int _putchar(char c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
