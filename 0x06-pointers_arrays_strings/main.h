50ffdba04f392555a94b45aa561bb4c4abffb956
Reinstorm <joemugoh215@gmail.com>
2023-03-02 21:21:18 -0800

    start again

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,14 @@
 
 #include <stdio.h>
 
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+char *_strdup(char *str);
+int _strcmp(char *s1, char *s2);
+char *rot13(char *);
 char *leet(char *);
-int _putchar(char);
+void print_number(int n);
+int _putchar(char c);
 
 #endif /* MAIN_H */
